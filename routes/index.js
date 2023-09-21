d3b45097c642e0ebc8fdcccd94cfffa13bf6efb5
harshitkaushik003 <harshitkaushik394@gmail.com>
2023-09-21 23:28:33 +0530

    initialised like functionality

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ router.get('/', homeController.home);
 router.use('/users', require('./user'));
 router.use('/posts', require('./post'));
 router.use('/comments', require('./comment'));
-
+router.use('/likes', require('./like'));
 module.exports = router
