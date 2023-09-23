ea64815cb037ee368af37ce1e47edb14b108ed25
harshitkaushik003 <harshitkaushik394@gmail.com>
2023-09-23 18:01:04 +0530

    added some more ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,24 +1,83 @@
 {
-    console.log("Hello World!!!");
-    let createPost = function(){
-        let newPostForm = $('#new-post-form');
-        console.log(newPostForm);
-        newPostForm.submit(function (e) { 
+    // let createPost = function(){
+    //     let newPostForm = $('#new-post-form');
+    //     newPostForm.submit((e)=>{
+    //         e.preventDefault();
+    //         $.ajax({
+    //             type: "post",
+    //             url: "/posts/create",
+    //             data:newPostForm.serialize(),
+    //             success: function (response) {
+    //                 console.log(response);
+    //                 let newPost = newPostDom(response.data.post);
+    //                 $(".main-body").prepend(newPost);
+    //                 deletePost($('.delete-post-button', newPost));
+    //             }, error: (error)=>{
+    //                 console.log(`Error in creating a Post -> ${error.responseText}`);
+    //             }
+    //         });
+    //     })
+    // }
+    // let newPostDom = function(posts){
+    //     return $(`
+    //     <div id='post-${posts._id}' class="post-display">
+    //         <div class="delete-icon delete-post-button">
+    //             <a href="/posts/delete/${posts.id}">
+    //                 <i class="fa-solid fa-trash-can"></i>
+    //             </a>
+    //         </div>
+    //         <div class="user-name">
+    //             <p>
+    //                 ${posts.user.name}
+    //             </p>
+                
+                
+    //         </div>
+    //         <div class="post-content">
+    //             <p>
+    //                 ${posts.content}
+    //             </p>
+    //         </div>
+    //         <div class="interactions">
+    //             <i class="fa-regular fa-heart"></i>
+    //         </div>
+    //         <div class="comment-section">
+    //             <form action="/comments/create" method="POST">
+    //                 <input type="text" class="comment-content" name="content" placeholder="add a comment" required>
+    //                 <input type="hidden" name="post" value="<%= posts._id %>">
+    //                 <input type="submit" value="Add">
+    //             </form>
+    //             <div class="comment-list">
+    //                 <div id="post-comments-${posts._id}">
+                        
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>    
+
+    //     `)
+    // }
+
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
             e.preventDefault();
             $.ajax({
-                type: "post",
-                url: "/posts/create",
-                data: newPostForm.serialize(),
+                type: "get",
+                url: $(deleteLink).prop('href'),
                 success: function (response) {
                     console.log(response);
-                    window.location.href = "/";
-                },
-                error: function(error){
-                    console.log(error.responseText);
+                    $(`#post-${response.data.post_id}`).remove();
+                },error: (error)=>{
+                    console.log(`error while deleting a post ${error.responseText}`);
                 }
             });
-        });
+        })
     }
-
-    createPost();   
+    // console.log($('.delete-post-button'));
+    for(let i of $('.delete-post-button')){
+        console.log(i);
+        deletePost(i);
+    }
+    
+    // createPost();
 }
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -12,19 +12,26 @@ module.exports.post = function(req, res){
 }
 
 module.exports.create = async function(req, res){
+    // let redirectToRoot = false;
     try {
         const response = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-        if(req.xhr){
-            return res.status(200).json({
-                data:{
-                    post: response
-                },
-                message: "Post Created"
-            })
-        }
+        // if(req.xhr){
+        //     req.flash('success', 'Post Created Successfully');
+        //     redirectToRoot = true;
+        //     return res.status(200).json({
+        //         data: {
+        //             post: response
+        //         },
+        //         message: "Post Created Successfully"
+        //     })
+        // }
+        // if(redirectToRoot){
+        //     return res.redirect('/');
+        // }
+        req.flash('success', 'Post Created Successfully');
         return res.redirect('/');
     } catch (error) {
         console.log(`Error while creating a post \n ${error}`);   
@@ -37,12 +44,21 @@ module.exports.destroy = async function(req,res){
         if(post.user == req.user.id){
             post.deleteOne();
             Comment.deleteMany({post: req.params.id});
+            if(req.xhr){
+                // console.log("reached at this point");
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post Deleted"
+                })
+            }
             return res.redirect('back');
         }else{
             return res.redirect('back');
         }      
     } catch (error) {
-        console.log(`error in creating post\n${error}`);
+        console.log(`error in deleting a post\n${error}`);
         return res.redirect('back');
     }
 
